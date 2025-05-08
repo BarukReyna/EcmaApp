8724310d7a6729433dbedf4c37f5cbca929f5f57
DANNA <193110126@utvam.edu.mx>
2025-05-08 11:37:09 -0600

    Creacion del proyecto

---
+++ src/main/java/com/baruk/ecma/Ecma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.baruk.ecma;
+
+/**
+ *
+ * @author ikram
+ */
+public class Ecma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
